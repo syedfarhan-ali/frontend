563d0bac8a44a21899d93aeac7fa3036e4c008e6
syedfarhan-ali <syedfarhanali012005@gmail.com>
2026-01-02 17:26:07 +0530

    last commit was a mistake i finised products page last time this time pricing is d one

--- src/landing_page/pricing/PricingPage.js
+++ src/landing_page/pricing/PricingPage.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import Hero from './Hero';
 import Brokerage from './Brokerage';
-
 import OpenAccount from '../OpenAccount'
-import Navbar from '../Navbar'
-import Footer from '../Footer'
 
 function PricingPage() {
     return ( 
