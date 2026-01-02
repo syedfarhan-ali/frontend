450a148cb6cdd9182d572e9f2d987fad515a84a2
syedfarhan-ali <syedfarhanali012005@gmail.com>
2026-01-02 15:55:06 +0530

    completed pricing section

--- src/landing_page/products/ProductPage.js
+++ src/landing_page/products/ProductPage.js
@@ -17,7 +17,12 @@ function ProductPage() {
         googlePlay=""
         appStore=""
       />
-      <RightSection />
+      <RightSection 
+        imageURL="media/images/console.png"
+        productName="Console"
+        productDescription="The central dashboard for your Zerodha account. Gain insights into your trades and investments with in-depth reports and visualisations."
+        learnMore=""
+      />
       <LeftSection
         imageURL="media/images/coin.png"
         productName="Coin"
@@ -27,7 +32,12 @@ function ProductPage() {
         googlePlay=""
         appStore=""
       />
-      <RightSection />
+      <RightSection 
+        imageURL="media/images/kiteconnect.png"
+        productName="Kite Connect API"
+        productDescription="Build powerful trading platforms and experiences with our super simple HTTP/JSON APIs. If you are a startup, build your investment app and showcase it to our clientbase."
+        learnMore=""
+      />
       <LeftSection
         imageURL="media/images/varsity.png"
         productName="Varsity Mobile"
@@ -37,7 +47,9 @@ function ProductPage() {
         googlePlay=""
         appStore=""
       />
-      
+      <p className="text-center mt-5 mb-5">
+        Want to know more about our technology stack? Check out the Zerodha.tech blog.
+      </p>
       <Universe />
     </>
   );
--- src/landing_page/products/RightSection.js
+++ src/landing_page/products/RightSection.js
@@ -1,8 +1,30 @@
 import React from 'react';
 
-function RightSection() {
+function RightSection({
+     //destructuring props here
+  imageURL,
+  productName,
+  productDescription,
+  learnMore,
+}) {
     return ( 
-        <h1>RightSection</h1>
+       <div className="container mt-5">
+      <div className="row">
+        
+        <div className="col-6 p-5 mt-5">
+          <h1>{productName}</h1>
+          <p>{productDescription}</p>
+          <div>
+            <a href={learnMore}>
+              Learn More
+            </a>
+          </div>
+        </div>
+        <div className="col-6">
+          <img src={imageURL} />
+        </div>
+      </div>
+    </div>
      );
 }
 
